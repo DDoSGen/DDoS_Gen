bb7a539c5b486d47b63e7531a4a20e2882ef5ea8
rltjfkrhgo <rltjfkrhgo@hanmail.net>
2020-08-07 17:11:18 +0900

    0807 1711

--- src/atkmod.cpp
+++ src/atkmod.cpp
@@ -22,7 +22,10 @@ void ATTACKMODULE::attack(){
 // TODO:make attack_routine function
 void* attack_routine(void* arg){
 
+
+    // TCP SYN ATTACK 작성중...
     ATTACKMODULE* module = (ATTACKMODULE*)arg;
+
 
     // get my mac, ip
     // send arp request to get target mac
@@ -30,14 +33,36 @@ void* attack_routine(void* arg){
     // packet for TCP SYN ATTACK
     PKT<ETHIPTCP> packet;
 
-    // set packet
+    // 시드생성
+    libnet_seed_prand(NULL);
+
+    // setting packet
+
     packet.pkt.eth_hdr.ether_dhost = target mac;
-    packet.pkt.eth_hdr.ether_shost = random mac;
+    // random source mac
+    packet.pkt.eth_hdr.ether_shost[0] = libnet_get_prand(LIBNET_PR8);
+    packet.pkt.eth_hdr.ether_shost[1] = libnet_get_prand(LIBNET_PR8);
+    packet.pkt.eth_hdr.ether_shost[2] = libnet_get_prand(LIBNET_PR8);
+    packet.pkt.eth_hdr.ether_shost[3] = libnet_get_prand(LIBNET_PR8);
+    packet.pkt.eth_hdr.ether_shost[4] = libnet_get_prand(LIBNET_PR8);
+    packet.pkt.eth_hdr.ether_shost[5] = libnet_get_prand(LIBNET_PR8);
     packet.pkt.eth_hdr.ether_type = htons(ETHERTYPE_IP);
 
-    packet.pkt.ip_hdr.ip_src = random ip;
+
+
+    packet.pkt.ip_hdr.ip_v = 4 << 4;                           // version
+    packet.pkt.ip_hdr.ip_hl = 5;                               // header length
+    packet.pkt.ip_hdr.ip_tos = IPTOS_LOWDELAY;                 // 뭔지 모르겠음
+    packet.pkt.ip_hdr.ip_len = htons();                        // total length
+    packet.pkt.ip_hdr.ip_id = libnet_get_prand(LIBNET_PRu16);  // random ID
+    packet.pkt.ip_hdr.ip_off = IP_DF;                          // don't fragment
+    packet.pkt.ip_hdr.ip_ttl = libnet_get_prand(LIBNET_PR8);   // random TTL
+    packet.pkt.ip_hdr.ip_p = IPPROTO_TCP;                      // protocol = TCP
+    packet.pkt.ip_hdr.ip_sum = htons();                        // 체크섬, 모르겠음
+    packet.pkt.ip_hdr.ip_src = libnet_get_prand(LIBNET_PRu32);  // random source IP
     packet.pkt.ip_hdr.ip_dst = target ip;
 
+    
 
     return NULL;
 }
