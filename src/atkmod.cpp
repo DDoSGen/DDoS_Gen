4695c990cda1b5b9771e4e3da7ecadbad66ee6fb
rltjfkrhgo <rltjfkrhgo@github.com>
2020-08-07 16:27:25 +0900

    0807 1627

--- src/atkmod.cpp
+++ src/atkmod.cpp
@@ -11,7 +11,7 @@ void ATTACKMODULE::attack(){
     pthread_t AttackThreads[THREADS];
     
     for(int i = 0; i < THREADS; i++){
-        pthread_create(&AttackThreads[i], NULL, attack_routine, NULL);
+        pthread_create(&AttackThreads[i], NULL, attack_routine, this);
     }
     
     for(int i = 0; i < THREADS; i++){
@@ -21,6 +21,23 @@ void ATTACKMODULE::attack(){
 
 // TODO:make attack_routine function
 void* attack_routine(void* arg){
+
+    ATTACKMODULE* module = (ATTACKMODULE*)arg;
+
+    // get my mac, ip
+    // send arp request to get target mac
+    
+    // packet for TCP SYN ATTACK
+    PKT<ETHIPTCP> packet;
+
+    // set packet
+    packet.pkt.eth_hdr.ether_dhost = target mac;
+    packet.pkt.eth_hdr.ether_shost = random mac;
+    packet.pkt.eth_hdr.ether_type = htons(ETHERTYPE_IP);
+
+    packet.pkt.ip_hdr.ip_src = random ip;
+    packet.pkt.ip_hdr.ip_dst = target ip;
+
 
     return NULL;
 }
